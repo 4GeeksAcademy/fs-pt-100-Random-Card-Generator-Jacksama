c7b1cb30026e191a7db36fd72d62d1c0367720c1
Jacksson Sánchez <info@creativoz.es>
2025-03-28 10:08:49 +0000

    Cartas Poker Jack

--- src/app.js
+++ src/app.js
@@ -1,10 +1,6 @@
 import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
 
 const paloUp = document.querySelector('#palo-up');
